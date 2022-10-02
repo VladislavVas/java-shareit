5a0b14379eb98347b64336fd1bb3253b0cec072e
VladislavVas <tenche@yandex.ru>
2022-10-02 19:09:00 +0300

    add controllers 40

--- src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
@@ -1,18 +1,42 @@
 package ru.practicum.shareit.item.dto;
 
-import org.mapstruct.InjectionStrategy;
-import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-@Mapper(componentModel = "spring", uses = ItemMapper.class, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
-public interface ItemMapper {
-    Item toItem(ItemDto dto);
+@Component
+public class ItemMapper {
+    public static Item toItem(ItemDto dto) {
+        return Item.builder()
+                .id(dto.getId())
+                .name(dto.getName())
+                .description(dto.getDescription())
+                .available(dto.getAvailable())
+                .build();
+    }
 
-    ItemDto toDto(Item item);
+    public static ItemDto toDto(Item item) {
+        return ItemDto.builder()
+                .id(item.getId())
+                .name(item.getName())
+                .description(item.getDescription())
+                .available(item.getAvailable())
+                .build();
+    }
 
-    List<Item> toListItem(List<ItemDto> dtoList);
+    public static List<Item> toListItem(List<ItemDto> dtoList) {
+        return dtoList.stream()
+                .map(ItemMapper::toItem)
+                .collect(Collectors.toList());
 
-    List<ItemDto> toDtoList(List<Item> itemList);
+    }
+
+    public static List<ItemDto> toDtoList(List<Item> itemList) {
+        return itemList.stream()
+                .map(ItemMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
 }
--- src/main/java/ru/practicum/shareit/user/dto/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserMapper.java
@@ -1,18 +1,38 @@
 package ru.practicum.shareit.user.dto;
 
-import org.mapstruct.InjectionStrategy;
-import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 import ru.practicum.shareit.user.model.User;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-@Mapper(componentModel = "spring", uses = UserMapper.class, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
-public interface UserMapper {
-    User toUser(UserDto dto);
+@Component
+public class UserMapper {
+    public static User toUser(UserDto dto) {
+        return User.builder()
+                .id(dto.getId())
+                .name(dto.getName())
+                .email(dto.getEmail())
+                .build();
+    }
 
-    UserDto toDto(User user);
+    public static UserDto toDto(User user) {
+        return UserDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .build();
+    }
 
-    List<User> toListUser(List<UserDto> dtoList);
+    public static List<User> toListUser(List<UserDto> dtoList) {
+        return dtoList.stream()
+                .map(UserMapper::toUser)
+                .collect(Collectors.toList());
+    }
 
-    List<UserDto> toDtoList(List<User> userList);
+    public static List<UserDto> toDtoList(List<User> userList) {
+        return userList.stream()
+                .map(UserMapper::toDto)
+                .collect(Collectors.toList());
+    }
 }
