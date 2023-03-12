a0853ca2d740ceb175d4139a50ec46c9c7b0653a
VladislavVas <tenche@yandex.ru>
2023-03-12 18:45:47 +0300

    add docker 7

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/Create.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/Create.java
@@ -1,4 +1,4 @@
 package ru.practicum.shareit.booking.dto;
 
-public class Create {
+public interface Create {
 }
--- gateway/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
@@ -2,14 +2,14 @@ package ru.practicum.shareit.item.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import ru.practicum.shareit.booking.dto.Create;
 
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
